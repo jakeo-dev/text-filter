49735962e78a8c6661ab4150067085086b9598d3
Jake O <56329025+jakeorch@users.noreply.github.com>
2022-09-17 07:28:35 -0700

    v1.0.2 - New filter to remove words above a certain length

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
-console.log('v1.0.1');
-console.log('whats new: \n Fixed bug involving removing just the words');
+console.log('v1.0.2');
+console.log('whats new: \n Added filter to remove words above a certain length');
 
 let badList = [];
 let final = '';
 let rgx = '';
+let finalRgx;
+let filter = selectFilter.selectedIndex;
+let longLim;
 
 function enter() {
-    if (badList.length < 1 || document.getElementById('input').value == '') {
-        alert('Enter at least one word and text');
-    } else {
-        for (var i = 0; i < badList.length; i++) {
-            if (document.getElementById('selectFilter').value == 1) {
-                if (rgx == '') {
-                    rgx = badList[i];
-                } else {
-                    rgx = `${rgx}|${badList[i]}`;
-                }
-            } else if (document.getElementById('selectFilter').value == 2) {
-                if (rgx == '') {
-                    rgx = `([^\\s]+${badList[i]}[^\\s]+|${badList[i]}[^\\s]+|[^\\s]+${badList[i]}|${badList[i]})(\\s|)`;
-                } else {
-                    rgx = `(${rgx}|[^\\s]+${badList[i]}[^\\s]+|${badList[i]}[^\\s]+|[^\\s]+${badList[i]}|${badList[i]})(\\s|)`;
+    if (filter === 0 || filter === 1) {
+        if (badList.length < 1 || document.getElementById('input').value == '') {
+            alert('Enter at least one word and text');
+        } else {
+            for (var i = 0; i < badList.length; i++) {
+                if (filter === 0) {
+                    if (rgx == '') {
+                        rgx = badList[i];
+                    } else {
+                        rgx = `${rgx}|${badList[i]}`;
+                    }
+                } else if (filter === 1) {
+                    if (rgx == '') {
+                        rgx = `([^\\s]+${badList[i]}[^\\s]+|${badList[i]}[^\\s]+|[^\\s]+${badList[i]}|${badList[i]})(\\s|)`;
+                    } else {
+                        rgx = `(${rgx}|[^\\s]+${badList[i]}[^\\s]+|${badList[i]}[^\\s]+|[^\\s]+${badList[i]}|${badList[i]})(\\s|)`;
+                    }
                 }
             }
+
+            regex();
         }
-
-        var finalRgx = new RegExp(rgx, 'gi');
-        final = document.getElementById('input').value.replace(finalRgx, '');
-        // final = final.replace(/(\n\s*\n)+/g, '\n'); // removes multi lines breaks to one line break
-
-        document.getElementById('finalText').innerText = final;
-
-        if (final == '') {
-            alert('Nothing to output');
+    } else if (filter === 2) {
+        if (longLim == '' || longLim == undefined) {
+            alert('Enter a character limit');
+        } else {
+            rgx = `\\w{${+longLim + +1},}`;
+            regex();
         }
+    }
+}
+
+function regex() {
+    finalRgx = new RegExp(rgx, 'gi');
+    final = document.getElementById('input').value.replace(finalRgx, '');
+    // final = final.replace(/(\n\s*\n)+/g, '\n'); // removes multi lines breaks to one line break
+
+    document.getElementById('finalText').innerText = final;
+
+    if (final == '') {
+        alert('Nothing to output');
     }
 }
 
 function add() {
     if (document.getElementById('inp').value == '') {
-        alert('Enter a word');
+        alert('Enter a string');
     } else {
         var li = document.createElement('li');
         var inputValue = document.getElementById('inp').value;
@@ -60,9 +76,41 @@ function add() {
     }
 }
 
+function addLim() {
+    if (document.getElementById('inpLim').value == '') {
+        alert('Enter a number');
+    } else if (document.getElementById('inpLim').value % 1 !== 0 || document.getElementById('inpLim').value < 1) {
+        alert('Enter a positive whole number');
+    } else {
+        longLim = document.getElementById('inpLim').value;
+        document.getElementById('limit').innerText = longLim + ' characters maximum';
+
+        document.getElementById('listDiv').style.display = 'flex';
+        document.getElementById('limDiv').style.marginBottom = '15px';
+
+        document.getElementById('inpLim').value = '';
+    }
+}
+
 function changeFilter() {
+    filter = selectFilter.selectedIndex;
+
+    if (filter === 2) {
+        document.getElementById('limDiv').style.display = 'flex';
+        document.getElementById('addDiv').style.display = 'none';
+        document.getElementById('list').style.display = 'none';
+        document.getElementById('limit').style.display = 'block';
+    } else {
+        document.getElementById('limDiv').style.display = 'none';
+        document.getElementById('addDiv').style.display = 'flex';
+        document.getElementById('list').style.display = 'block';
+        document.getElementById('limit').style.display = 'none';
+    }
+
     rgx = '';
 }
+
+window.onload = changeFilter;
 
 function copy() {
     navigator.clipboard.writeText(document.getElementById('finalText').innerText);
@@ -88,7 +136,11 @@ document.getElementById("list").addEventListener("click", function (e) {
 });
 
 document.body.onkeyup = function (event) {
-    if (event.keyCode == 13 && document.getElementById('inp') === document.activeElement) {
-        document.getElementById('addB').click(); // clicks enter
+    if (event.keyCode == 13) {
+        if (document.getElementById('inp') === document.activeElement) {
+            document.getElementById('addB').click(); // clicks add
+        } else if (document.getElementById('inpLim') === document.activeElement) {
+            document.getElementById('addLim').click(); // clicks enter
+        }
     }
 }
